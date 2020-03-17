89443195e4a0ef7b3937d1dbb3f923b9b4dbad3f
Frantisek Hartman <frant.hartm@gmail.com>
2020-03-17 09:42:09 +0100

    Initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/JetJob.java
@@ -0,0 +1,23 @@
+package ${package};
+
+import com.hazelcast.jet.Jet;
+import com.hazelcast.jet.JetInstance;
+import com.hazelcast.jet.pipeline.Pipeline;
+import com.hazelcast.jet.pipeline.Sinks;
+import com.hazelcast.jet.pipeline.test.TestSources;
+
+public class JetJob {
+
+    public static void main(String[] args) {
+        Pipeline p = Pipeline.create();
+        p.readFrom(TestSources.itemStream(10))
+         .withoutTimestamps()
+         .filter(event -> event.sequence() % 2 == 0)
+         .setName("filter out odd numbers")
+         .writeTo(Sinks.logger());
+
+        JetInstance jet = Jet.newJetInstance();
+        jet.newJob(p).join();
+    }
+
+}
